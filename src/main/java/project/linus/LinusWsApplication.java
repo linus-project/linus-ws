4b35c88fff978f50e3e348aebdbf16b00315b750
noisyboy-official <gabriel.osilva@bandtec.com.br>
2022-08-12 17:22:52 -0300

    feature: implementing project initial structure

---
+++ src/main/java/project/linus/LinusWsApplication.java
@@ -0,0 +1,13 @@
+package project.linus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LinusWsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LinusWsApplication.class, args);
+	}
+
+}
