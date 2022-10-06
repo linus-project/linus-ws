6b99529f015f946db59548b8a4fa1fbc6f34b4d4
noisyboy-official <gabriel.osilva@bandtec.com.br>
2022-10-05 21:28:10 -0300

    feature: implementing user password manager

---
+++ src/main/java/project/linus/model/user/manager/AdminPasswordManager.java
@@ -0,0 +1,18 @@
+package project.linus.model.user.manager;
+
+public class AdminPasswordManager extends PasswordManager{
+    private String adminKey;
+
+    public AdminPasswordManager(String username, String password, String newPassword, String adminKey) {
+        super(username, password, newPassword);
+        this.adminKey = adminKey;
+    }
+
+    public String getAdminKey() {
+        return adminKey;
+    }
+
+    public void setAdminKey(String adminKey) {
+        this.adminKey = adminKey;
+    }
+}
---
+++ src/main/java/project/linus/model/user/manager/UserPasswordManager.java
@@ -0,0 +1,8 @@
+package project.linus.model.user.manager;
+
+public class UserPasswordManager extends PasswordManager {
+
+    public UserPasswordManager(String username, String password, String newPassword) {
+        super(username, password, newPassword);
+    }
+}
