45cf9aaa759ddb1aa7be04eff27e57344e1003ed
Raquel <raquelogs.1202@hotmail.com>
2022-10-09 21:01:18 -0300

    Revert "feature: implementing cors"
    This reverts commit 0c44d3bdf75e92c669f79c6bc14355e5e9052be7.

--- src/main/java/project/linus/LinusWsApplication.java
+++ src/main/java/project/linus/LinusWsApplication.java
@@ -1,37 +1,13 @@
 package project.linus;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.core.env.Environment;
-import org.springframework.web.servlet.config.annotation.CorsRegistration;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @SpringBootApplication
 public class LinusWsApplication {
 
-	@Autowired
-	private Environment env;
-
 	public static void main(String[] args) {
 		SpringApplication.run(LinusWsApplication.class, args);
-	}
-
-	@Bean
-	public WebMvcConfigurer corsConfigurer() {
-		return new WebMvcConfigurerAdapter() {
-			@Override
-			public void addCorsMappings(CorsRegistry registry) {
-				String urls = env.getProperty("cors.urls");
-				CorsRegistration reg = registry.addMapping("/**");
-				for(String url: urls.split(",")) {
-					reg.allowedOrigins(url);
-				}
-			}
-		};
 	}
 
 }
